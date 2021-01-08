739f7872419dfe191c25d3a50137ddf985415110
Tarcisio <tarcisio.angelo@gmail.com>
2021-01-08 17:25:18 -0300

    Atualizando rota de update

--- routes/routes.go
+++ routes/routes.go
@@ -12,8 +12,8 @@ func Init() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/login", ControllerUser.Login).Methods("POST")
 	router.HandleFunc("/user", ControllerUser.List).Methods("GET")
+	router.HandleFunc("/user", ControllerUser.Update).Methods("PUT")
 	router.HandleFunc("/user/{id}", MiddlewareAuth.Auth(ControllerUser.Find)).Methods("GET")
-	router.HandleFunc("/user/{id}", ControllerUser.Create).Methods("POST")
 	router.HandleFunc("/user/{id}", ControllerUser.Delete).Methods("DELETE")
 	return router
 }
